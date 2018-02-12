dae6293a4d439c8187a3e2529bb193e1306ab86f
hikari8807 <checkingmymail@gmail.com>
2018-02-13 00:19:21 +0800

    Complete code for ViewController

--- CoursePickerView/ViewController.swift
+++ CoursePickerView/ViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        courses = ["OOAD", "JAVA", "PMIS"];
+
+        let pickerView : UIPickerView = UIPickerView();
+        pickerView.delegate = self;
+        pickerView.dataSource = self;
+        textFieldCourse.inputView = pickerView;
+    }
+    
+    @IBOutlet weak var textFieldCourse: UITextField!
+    var courses: Array<String>?;
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return course!.count;
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return course![row];
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component:Int){
+        textFieldCourse.text = course![row];
     }
 
     override func didReceiveMemoryWarning() {
